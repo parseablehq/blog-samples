3ab61143f524dd0fdf984f1239f1af34743ef1b0
balaji-jr <balaji.joshua1998@gmail.com>
2024-08-07 20:01:20 +0530

    food delivery use case with temporal workflow

--- temporal-activity-blog/src/activities.ts
+++
@@ -1,9 +0,0 @@
-import { log } from '@temporalio/activity';
-
-export async function addReminderToDatabase(_text: string): Promise<void> {
-  log.info('Adding reminder record to the database');
-}
-
-export async function notifyUser(text: string): Promise<void> {
-  log.info('Notifying user', { text });
-}
